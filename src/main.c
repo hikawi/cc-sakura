e649cc4a776359423574b3e58a4cbaca476758ae
tsukimi <128211112+hikawi@users.noreply.github.com>
2025-06-30 14:29:51 +0700

    initial commit with deps

---
+++ src/main.c
@@ -0,0 +1,13 @@
+// main.c
+//
+// The main entrypoint of the program, any events handling, movements, app state
+// updating should be delegated to other source files. Do not do actual handling
+// in this file.
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello, World");
+  return 0;
+}
